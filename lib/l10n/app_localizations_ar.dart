1cc52aa13cfb42aba804815755f580143a928392
mohamedelkhateeb55 <melkhateeb552001@gmail.com>
2025-06-30 00:39:46 +0300

    + initialize project . + Setup Evently app with Firebase, Auth & Event features

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,179 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get language => 'اللغة';
+
+  @override
+  String get theme => 'السمة';
+
+  @override
+  String get arabic => 'العربية';
+
+  @override
+  String get english => 'الانجليزية';
+
+  @override
+  String get light => 'فاتح';
+
+  @override
+  String get dark => 'داكن';
+
+  @override
+  String get home => 'الرئيسية';
+
+  @override
+  String get map => 'الخريطة';
+
+  @override
+  String get favourite => 'المفضلة';
+
+  @override
+  String get profile => 'الشخصية';
+
+  @override
+  String get welcome_back => 'مرحبا';
+
+  @override
+  String get all => 'الكل';
+
+  @override
+  String get sport => ' الرياضة';
+
+  @override
+  String get birthday => 'عيد المبلاد';
+
+  @override
+  String get meeting => 'الاجتماع';
+
+  @override
+  String get gaming => 'الالعاب';
+
+  @override
+  String get workshop => 'ورش العمل';
+
+  @override
+  String get book_club => 'نادي القراءة';
+
+  @override
+  String get exhibition => 'المعرض';
+
+  @override
+  String get holiday => 'الاجازة';
+
+  @override
+  String get eating => 'الطعام';
+
+  @override
+  String get search_event => 'ابحث عن الحدث';
+
+  @override
+  String get logout => 'تسجيل الخروج';
+
+  @override
+  String get create_event => 'انشاء حدث';
+
+  @override
+  String get title => 'العنوان';
+
+  @override
+  String get event_title => 'عنوان الحدث';
+
+  @override
+  String get description => 'الوصف';
+
+  @override
+  String get event_description => 'وصف الحدث';
+
+  @override
+  String get event_date => 'تاريخ الحدث';
+
+  @override
+  String get event_time => 'وقت الحدث';
+
+  @override
+  String get choose_date => 'اختر التاريخ';
+
+  @override
+  String get choose_time => 'اختر الوقت';
+
+  @override
+  String get location => 'الموقع';
+
+  @override
+  String get choose_event_location => 'اختر موقع الحدث';
+
+  @override
+  String get add_event => 'اضافة حدث';
+
+  @override
+  String get please_enter_event_title => 'من فضلك ادخل عنوان الحدث';
+
+  @override
+  String get please_enter_event_description => 'من فضلك ادخل وصف الحدث';
+
+  @override
+  String get please_choose_date => 'من فضلك اختر التاريخ';
+
+  @override
+  String get please_choose_time => 'من فضلك اختر الوقت';
+
+  @override
+  String get email => 'البريد';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get forget_password => 'نسيت كلمة المرور';
+
+  @override
+  String get login => 'تسجيل الدخول';
+
+  @override
+  String get do_not_have_account => 'لا أملك حساب ؟';
+
+  @override
+  String get create_account => 'انشاء حساب';
+
+  @override
+  String get or => 'أو';
+
+  @override
+  String get name => 'الاسم';
+
+  @override
+  String get re_password => 'اعادة كلمة المرور';
+
+  @override
+  String get already_have_account => 'لدي حساب بالتأكيد';
+
+  @override
+  String get register => 'التسجيل';
+
+  @override
+  String get no_events_founded => 'لا يوجد احداث';
+
+  @override
+  String get no_favourite_events_founded => 'لا يوجد احداث مفضلة';
+
+  @override
+  String get login_with_google => 'التسجيل بحساب جوجل';
+
+  @override
+  String get edit_event => 'تعديل الحدث';
+
+  @override
+  String get update_event => 'تحديث الحدث';
+
+  @override
+  String get save_changes => 'حفظ التغييرات';
+}
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,179 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get theme => 'Theme';
+
+  @override
+  String get arabic => 'Arabic';
+
+  @override
+  String get english => 'English';
+
+  @override
+  String get light => 'Light';
+
+  @override
+  String get dark => 'Dark';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get map => 'Map';
+
+  @override
+  String get favourite => 'Favourite';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get welcome_back => 'Welcome Back';
+
+  @override
+  String get all => 'All';
+
+  @override
+  String get sport => ' Sport';
+
+  @override
+  String get birthday => 'Birthday';
+
+  @override
+  String get meeting => 'Meeting';
+
+  @override
+  String get gaming => 'Gaming';
+
+  @override
+  String get workshop => 'WorkShop';
+
+  @override
+  String get book_club => 'Book Club';
+
+  @override
+  String get exhibition => 'Exhibition';
+
+  @override
+  String get holiday => 'Holiday';
+
+  @override
+  String get eating => 'Eating';
+
+  @override
+  String get search_event => 'Search for Event';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get create_event => 'Create Event';
+
+  @override
+  String get title => 'Title';
+
+  @override
+  String get event_title => 'Event Title';
+
+  @override
+  String get description => 'Description';
+
+  @override
+  String get event_description => 'Event Description';
+
+  @override
+  String get event_date => 'Event Date';
+
+  @override
+  String get event_time => 'Event Time';
+
+  @override
+  String get choose_date => 'Choose Date';
+
+  @override
+  String get choose_time => 'Choose Time';
+
+  @override
+  String get location => 'Location';
+
+  @override
+  String get choose_event_location => 'Choose Event Location';
+
+  @override
+  String get add_event => 'Add Event';
+
+  @override
+  String get please_enter_event_title => 'Please enter event title';
+
+  @override
+  String get please_enter_event_description => 'Please enter event description';
+
+  @override
+  String get please_choose_date => 'Please choose date';
+
+  @override
+  String get please_choose_time => 'Please choose time';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get forget_password => 'Forget Password';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get do_not_have_account => 'Don’t Have Account ?';
+
+  @override
+  String get create_account => 'Create Account';
+
+  @override
+  String get or => 'Or';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get re_password => 'Re Password';
+
+  @override
+  String get already_have_account => 'Already Have Account ?';
+
+  @override
+  String get register => 'Register';
+
+  @override
+  String get no_events_founded => 'No Events Founded';
+
+  @override
+  String get no_favourite_events_founded => 'No Favourite Events Founded';
+
+  @override
+  String get login_with_google => 'Login With Google';
+
+  @override
+  String get edit_event => 'Edit Event';
+
+  @override
+  String get update_event => 'Update Event';
+
+  @override
+  String get save_changes => 'Save changes';
+}
